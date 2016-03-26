375558c201d5e5cfc4bb8368061e9d350e853cb6
José Manuel Condes <josemanuelcondesmoreno@gmail.com>
2016-03-26 21:30:06 +0100

    CommitA-4

--- src/ClaseA.java
+++ src/ClaseA.java
@@ -6,21 +6,29 @@ public class ClaseA {
 	
 	public ClaseA(){
 		this.setNombre("Jose");
-		this.apellidos = "Martinez";
+		this.setApellidos("Martinez");
 	}
-
-	public static void main(String[] args) {
-		
-		ClaseA ca = new ClaseA();
-
-	}
-
+	
 	public String getNombre() {
 		return nombre;
 	}
 
 	public void setNombre(String nombre) {
 		this.nombre = nombre;
+	}
+	
+	public String getApellidos() {
+		return apellidos;
+	}
+
+	public void setApellidos(String apellidos) {
+		this.apellidos = apellidos;
+	}	
+
+	public static void main(String[] args) {
+		
+		ClaseA ca = new ClaseA();
+
 	}
 
 }
